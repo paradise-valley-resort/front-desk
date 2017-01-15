93520d0885ee7f7f535e0216d2e05c613a9663df
Kyle Daugherty <kyle.daugherty@gmail.com>
2017-01-15 11:06:17 -0600

    Remove presence vaidation on user admin attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :admin, presence: true
   validates :first_name, length: { maximum: 255 }, presence: true
   validates :last_name, length: { maximum: 255 }, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ describe User do
     it { should validate_length_of(:first_name).is_at_most(255) }
     it { should validate_length_of(:last_name).is_at_most(255) }
 
-    it { should validate_presence_of(:admin) }
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
   end
